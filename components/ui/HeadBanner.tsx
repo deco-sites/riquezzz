5a3fa61aae442eff8b311bfd651abce0b11bc020
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-06-30 20:36:56 -0300

    fix: adjustment

--- components/ui/HeadBanner.tsx
+++ components/ui/HeadBanner.tsx
@@ -18,10 +18,10 @@ function HeadBanner({ srcDesktop, srcMobile, alt }: Props) {
 
   return (
     <div id={id} class="sm:px-4 w-full">
-      <div class="md:hidden flex flex-row justify-between items-center border-b border-base-200 w-full mx-auto">
+      <div class="md:hidden flex flex-row justify-between items-center border-b border-base-200 w-full  mb-1 mx-auto">
         <img class="w-full" src={srcMobile} alt={alt} />
       </div>
-      <div class="hidden md:flex flex-row justify-between items-center border-b border-base-200 w-full gap  mx-auto">
+      <div class="hidden md:flex flex-row justify-between items-center border-b border-base-200 w-full mb-2 gap  mx-auto">
         <img class="w-full" src={srcDesktop} alt={alt} />
       </div>
     </div>
