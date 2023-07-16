3fb6fbea2faee0c46ce3e979ef9445cc955511e7
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-07-16 19:25:28 -0300

    fix: Mobile and closed

--- components/ui/CookieConsent.tsx
+++ components/ui/CookieConsent.tsx
@@ -1,6 +1,5 @@
 import Button from "$store/components/ui/Button.tsx";
 import { useId } from "preact/hooks";
-import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 const script = (id: string) => `
 const callback = () => {
@@ -26,45 +25,40 @@ const callback = () => {
 window.addEventListener('scroll', callback, { once: true });
 `;
 
-export interface Props {
-  image?: LiveImage | undefined;
-}
-function CookieConsent({ image }: Props) {
+function CookieConsent() {
   const id = `cookie-consent-${useId()}`;
 
   return (
     <>
       <div
         id={id}
-        class="transform-gpu px-5  transition fixed top-0 w-screen z-50"
+        class="transform-gpu translate-y-[200%] transition fixed sm:top-0 bottom-4 w-screen z-50"
       >
-        <div class="container max-w-[500px] justify-center flex-row px-6 py-6 rounded-sm border  border-base-200 flex gap-4 items-start p-4 shadow bg-base-100">
-          <img
-            class="w-[100px] mr-4"
-            src={"https://dito-browser-notification-images.s3.amazonaws.com/MjAyMC0wMy0yNCAxMTo0OToxNyAtMDMwMEJhdyBDbG90aGluZyBPZmljaWFsODc5/modal/1587406327-logo%2080.jpg"}
-            alt="logo"
-          />
-          <div class="flex w-full flex-col  gap-2  rounded-xl ">
-            <span class="flex-grow text-lg font-medium text-black mb-2">
-              <div class="flex relative  justify-end">
-                <Button
-                  data-button-cc-close
-                  class="btn-outline mt-[-30px] mr-[-15px] hover:text-black text-black bg-transparent hover:bg-transparent absolute border-none max-h-[20px] w-[20px] p-0 m-0 rounded"
-                >
-                  X
-                </Button>
-              </div>
+        <div class="container  px-4 py-4 rounded border border-base-200 flex flex-col max-w-[90vw] sm:max-w-[400px] gap-4 items-start sm:items-center shadow bg-base-100">
+          <div class="flex flex-row">
+            <img
+              class="w-[50px] sm:w-[100px] mr-4"
+              src={"https://dito-browser-notification-images.s3.amazonaws.com/MjAyMC0wMy0yNCAxMTo0OToxNyAtMDMwMEJhdyBDbG90aGluZyBPZmljaWFsODc5/modal/1587406327-logo%2080.jpg"}
+              alt="logo"
+            />
+            <span class="flex-grow text-sm sm:text-lg font-medium text-black mb-2">
               Quer receber notificações das nossas campanhas, lançamentos e
               promoções antes de todo mundo?
             </span>
-            <div class="flex w-full justify-end">
-              <Button
-                class=" bg-[#444] rounded-sm text-white  max-h-[30px] py-0  px-5 w-[120px] border-none transition duration-75 hover:bg-[#7a7a7a]"
-                data-button-cc-accept
-              >
-                SIM!
-              </Button>
-            </div>
+          </div>
+          <div class="flex flex-row gap-2 w-full sm:w-auto">
+            <Button
+              data-button-cc-close
+              class="btn-outline rounded-sm max-h-[30px] py-0  mr-5 px-5 w-[120px] border-none transition duration-75 hover:bg-[#7a7a7a]"
+            >
+              NÃO
+            </Button>
+            <Button
+              data-button-cc-accept
+              class=" bg-[#444] rounded-sm text-white  max-h-[30px] py-0  px-5 w-[120px] border-none transition duration-75 hover:bg-[#7a7a7a]"
+            >
+              SIM!
+            </Button>
           </div>
         </div>
       </div>
