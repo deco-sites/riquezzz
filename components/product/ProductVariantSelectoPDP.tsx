09e0b78135481151bbb62f97c9c93ef0b95b8778
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-07-24 17:58:06 -0300

    fix: order of size in the pdp

--- components/product/ProductVariantSelectoPDP.tsx
+++ components/product/ProductVariantSelectoPDP.tsx
@@ -8,14 +8,35 @@ interface Props {
 
 function VariantSelector({ product, product: { url } }: Props) {
   const possibilities = useVariantPossibilities(product);
-  console.log(possibilities);
+
+  const nv = Object.entries(Object.values(possibilities)[0] ?? {});
+  const pppp = nv.find((sku) => sku[0] === "4P");
+  const ppp = nv.find((sku) => sku[0] === "3P");
+  const pp = nv.find((sku) => sku[0] === "PP");
+  const p = nv.find((sku) => sku[0] === "P");
+  const m = nv.find((sku) => sku[0] === "M");
+  const g = nv.find((sku) => sku[0] === "G");
+  const gg = nv.find((sku) => sku[0] === "GG");
+  const ggg = nv.find((sku) => sku[0] === "3G");
+  const gggg = nv.find((sku) => sku[0] === "4G");
+
+  const newVariants = [pppp, ppp, pp, p, m, g, gg, ggg, gggg];
+
+  const FNVariants: any = [];
+  newVariants.map((a) => {
+    if (a !== undefined) {
+      FNVariants.push(a);
+    }
+  });
+  const varintasFinish: [string, [string]] = FNVariants;
+
   return (
     <ul class="flex flex-col gap-4">
       {Object.keys(possibilities).map((name) => (
         <li class="flex flex-col gap-2">
           <span class="text-sm">{name}</span>
-          <ul class="flex flex-row gap-3 justify-start max-h-[20px]">
-            {Object.entries(possibilities[name]).map(([value, [link]]) => (
+          <ul class="flex flex-row gap-3 justify-start items-end max-h-[20px]">
+            {varintasFinish.map(([value, [link]]) => (
               <li>
                 <a href={link}>
                   <Avatar
