f4c95b4f455dd96d3bdee0a9c535afb725a39d20
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-07-29 14:59:21 -0300

    Lint

--- components/ui/BannerMovieImg.tsx
+++ components/ui/BannerMovieImg.tsx
@@ -16,7 +16,6 @@ export type BorderRadius =
   | "3xl"
   | "full";
 
-
 export interface BannerCampaing {
   /** @description RegExp to enable this banner on the current URL. Use /feminino/* to display this banner on feminino category  */
   matcher: string;
@@ -35,8 +34,8 @@ export interface BannerCampaing {
      * @description When you click you go to
      */
     hrefMovie?: string;
-  }
- 
+  };
+
   borderRadius: {
     /** @default none */
     mobile?: BorderRadius;
