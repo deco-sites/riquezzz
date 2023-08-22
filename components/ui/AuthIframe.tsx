6fb14a140a107bab0f2b022ecad54bbffb682ae0
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2023-08-22 15:39:03 -0300

    try fix iframe

--- components/ui/AuthIframe.tsx
+++ components/ui/AuthIframe.tsx
@@ -14,7 +14,7 @@ function AuthIframe({
       </header>
       <main class="w-full max-w-[80%] sm:max-w-[50%] text-center">
         <iframe
-          src={url}
+          src={"http://localhost"+url}
           class={`w-[100vw] text-center h-[500px]`}
           frameBorder="0"
           scrolling="no"
