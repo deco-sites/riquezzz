0e120e6aa7fdcd57f9f6e3aa7a70d85976be472e
Henrique Costa <riquegonsalves@gmail.com>
2023-08-24 20:41:57 -0300

    Fix/banners fields (#121)
    * banners fields fixes
    
    * format
    
    ---------
    
    Co-authored-by: riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>

--- components/ui/BannerFull.tsx
+++ components/ui/BannerFull.tsx
@@ -10,33 +10,42 @@ export interface Props {
 function BannerFull({ alt, desktop, mobile }: Props) {
   return (
     <div class="w-full px-auto lg:max-w-none sm:m-0 lg:overflow-hidden lg:pr-[40px]">
-      <Picture
-        preload
-        class="col-start-1 col-span-1 row-start-1 row-span-1"
-      >
-        {mobile
-          ? (
-            <Source
-              src={mobile}
-              width={2170}
-              height={1315}
-              class=""
-            />
-          )
-          : ("")}
-        {desktop
-          ? (
-            <Source
-              src={desktop}
-              width={2170}
-              height={1315}
-              class=""
-            />
-          )
-          : ("")}
-
-        {desktop ? <img class="w-full" src={desktop} alt={alt} /> : ("")}
-      </Picture>
+      <div class="hidden md:flex">
+        <Picture
+          preload
+          class="col-start-1 col-span-1 row-start-1 row-span-1"
+        >
+          {desktop
+            ? (
+              <Source
+                src={desktop}
+                width={1920}
+                height={1000}
+                class="hidden lg:flex"
+              />
+            )
+            : ("")}
+          {desktop ? <img class="w-full" src={desktop} alt={alt} /> : ("")}
+        </Picture>
+      </div>
+      <div class="flex md:hidden">
+        <Picture
+          preload
+          class="col-start-1 col-span-1 row-start-1 row-span-1"
+        >
+          {mobile
+            ? (
+              <Source
+                src={mobile}
+                width={720}
+                height={480}
+                class="flex lg:hidden"
+              />
+            )
+            : ("")}
+          {desktop ? <img class="w-full" src={desktop} alt={alt} /> : ("")}
+        </Picture>
+      </div>
     </div>
   );
 }
