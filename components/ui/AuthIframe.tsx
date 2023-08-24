6931253f3ba507f8d8169101974403e0f23f68e0
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2023-08-24 17:52:49 -0300

    test

--- components/ui/AuthIframe.tsx
+++ components/ui/AuthIframe.tsx
@@ -22,29 +22,33 @@ const runOnMount = () => {
     //   y: iFrame!.contentWindow!.document.body.scrollHeight,
     // });
 
-    const d = iFrame!.contentDocument!.documentElement!.getElementsByClassName("vtex-my-account-1-x-menuLink")[0]
-    let logout = false;
-    d.addEventListener("click", (e) => {
-      logout = true
-      console.log({ e })
-    })
     
-    console.log({ d })
-
-    iFrame.onload = (e) => {
-      const routes: IDictionary<string> = {
-        "Cartões": "/conta/cartoes",
-        "Endereços": "/conta/enderecos",
-        "Perfil": "/conta/perfil",
-        "Pedidos": "/conta/pedidos",
-        "Autenticação": "/conta/autenticacao",
-        "Login": "/",
-      };
-      window.location.href = logout ? "/" : (routes[iFrame.title] || "/");
-    };
 
     iFrame.height = iFrame!.contentWindow!.document.body.scrollHeight
       .toString() + "px";
+
+      const d = iFrame!.contentDocument!.documentElement!.getElementsByClassName(
+        "vtex-my-account-1-x-menuLink",
+      )[0];
+      let logout = false;
+      d.addEventListener("click", (e) => {
+        logout = true;
+        console.log({ e });
+      });
+  
+      console.log({ d });
+  
+      iFrame.onload = (e) => {
+        const routes: IDictionary<string> = {
+          "Cartões": "/conta/cartoes",
+          "Endereços": "/conta/enderecos",
+          "Perfil": "/conta/perfil",
+          "Pedidos": "/conta/pedidos",
+          "Autenticação": "/conta/autenticacao",
+          "Login": "/",
+        };
+        window.location.href = logout ? "/" : (routes[iFrame.title] || "/");
+      };
 
     // console.log({ iframeHeight: iFrame.height });
     // console.log("Foi");
