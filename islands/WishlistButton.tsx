d57ece834335917c145feded9035f9f9ec984c55
Guilherme Baufaker <baufaker@gmail.com>
2023-08-30 11:52:46 -0300

    fixed wishlist button import

--- islands/WishlistButton.tsx
+++ islands/WishlistButton.tsx
@@ -1,6 +1,7 @@
-import WishlistButton from "$store/components/wishlist/WishlistButton.tsx";
-import type { Props } from "$store/components/wishlist/WishlistButton.tsx";
+export { default } from "$store/components/wishlist/WishlistButton.tsx";
+// import WishlistButton from "$store/components/wishlist/WishlistButton.tsx";
+// import type { Props } from "$store/components/wishlist/WishlistButton.tsx";
 
-export default function WishlistButtonIsland(props: Props) {
-  return <WishlistButton {...props} />;
-}
+// export default function WishlistButtonIsland(props: Props) {
+//   return <WishlistButton {...props} />;
+// }
