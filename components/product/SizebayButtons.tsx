2f68f3e4354c2d08470a2601f555374aa2ab4069
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2023-08-31 23:45:12 -0300

    remove island

--- components/product/SizebayButtons.tsx
+++ components/product/SizebayButtons.tsx
@@ -1,7 +1,6 @@
-import Button from "$store/components/ui/Button.tsx";
 import { useState } from "preact/hooks";
 import Modal from "$store/components/ui/Modal.tsx";
-import { lazy, Suspense } from "preact/compat";
+import { Suspense } from "preact/compat";
 import Loading from "$store/components/ui/Loading.tsx";
 import SizebayIframe from "deco-sites/riquezzz/components/ui/SizebayIframe.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
